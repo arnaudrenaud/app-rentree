43ec0059fd4d90df374468f038e2f5c8e7a2873e
Arnaud Renaud <arnaud.renaud@gmail.com>
2021-09-17 14:59:45 +0200

    Add button to show or hide form

--- web-client/src/components/CreateWilderForm.js
+++ web-client/src/components/CreateWilderForm.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 import * as styled from "./CreateWilderForm.styled";
 
 const CreateWilderForm = () => {
+  const [shown, setShown] = useState(true);
   const [name, setName] = useState("");
   const [city, setCity] = useState("Bordeaux");
   const [status, setStatus] = useState(null);
@@ -26,37 +27,48 @@ const CreateWilderForm = () => {
 
   return (
     <styled.Container>
-      <form onSubmit={submitForm}>
-        <label>
-          Name:{" "}
-          <input
-            type="text"
-            name="name"
-            value={name}
-            onChange={(event) => {
-              setName(event.currentTarget.value);
-            }}
-          />
-        </label>
-        <br />
-        <label>
-          City :{" "}
-          <input
-            type="text"
-            name="city"
-            value={city}
-            onChange={(event) => {
-              setCity(event.currentTarget.value);
-            }}
-          />
-        </label>
-        <br />
-        <input type="submit" />
-      </form>
-      {status && (
-        <styled.StatusMessage success={status.success}>
-          {status.message}
-        </styled.StatusMessage>
+      <button
+        onClick={() => {
+          setShown(!shown);
+        }}
+      >
+        {shown ? "Cacher le formulaire" : "Afficher le formulaire"}
+      </button>
+      {shown && (
+        <>
+          <form onSubmit={submitForm}>
+            <label>
+              Name:{" "}
+              <input
+                type="text"
+                name="name"
+                value={name}
+                onChange={(event) => {
+                  setName(event.currentTarget.value);
+                }}
+              />
+            </label>
+            <br />
+            <label>
+              City :{" "}
+              <input
+                type="text"
+                name="city"
+                value={city}
+                onChange={(event) => {
+                  setCity(event.currentTarget.value);
+                }}
+              />
+            </label>
+            <br />
+            <input type="submit" />
+          </form>
+          {status && (
+            <styled.StatusMessage success={status.success}>
+              {status.message}
+            </styled.StatusMessage>
+          )}
+        </>
       )}
     </styled.Container>
   );
